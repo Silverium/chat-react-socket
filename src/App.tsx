c41b6b10f742a01ae74d7d6f22e7d00f9d7bdb7c
Silverium <soldeplatadeveloper@gmail.com>
2020-05-20 16:03:54 +0200

    add linter

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import './App.scss'
 
-const App:React.StatelessComponent<{}>  = () => (
-    <div className='container'>
-        <h1>Hello World, React!</h1>
-    </div>
+const App: React.StatelessComponent<{}> = () => (
+  <div className='container'>
+    <h1>Hello World, React!</h1>
+  </div>
 )
 
-export default App;
+export default App
