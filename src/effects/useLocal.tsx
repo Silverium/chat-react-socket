83b0bd27fc74538125f84da31c88763467371e81
Silverium <soldeplatadeveloper@gmail.com>
2020-05-21 21:20:04 +0200

    add jsdoc to useLocal

--- src/effects/useLocal.tsx
+++ src/effects/useLocal.tsx
@@ -1,7 +1,15 @@
 /* global localStorage */
 import React from 'react'
+/**
+ * Effect that wraps React.useState hook and syncs the state with local storage
+ *
+ * It has to be bound to "this", like in the following example
+ * ```
+ *  const [lang, setLang] = useLocal.bind(this)(LANGUAGE)
+ * ```
+ * @param localStorageKey key to save and retrieve from localStorage
+ */
 export const useLocal = (localStorageKey: string) => {
-  console.log(localStorageKey)
   const [value, setValue] = React.useState(
     localStorage.getItem(localStorageKey) || ''
   )
