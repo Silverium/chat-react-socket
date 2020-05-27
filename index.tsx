3728e690e60768bfbaf3a103ddf35af311c993ef
Silverium <soldeplatadeveloper@gmail.com>
2020-05-27 15:03:52 +0200

    improve webpack chunks

--- index.tsx
+++ index.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom'
 
 import Loading from './src/components/Loading'
 
-import './src/i18n'
 // import App from './src/containers/App'
-const App = React.lazy(() => import('./src/containers/App'))
+const App = React.lazy(() => import(/* webpackChunkName: "content"  */ './src/containers/App'))
 
 ReactDOM.render(
   <Suspense fallback={<Loading />}>
--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import each from 'lodash-es/each'
 import { useTranslation } from 'react-i18next'
 
+import '../../i18n'
 import './App.scss'
 import { useLocal, useChatMessage, useChatHistory, useWindowSize } from '@/effects'
 import {
@@ -11,9 +12,10 @@ import { SettingsContext } from '@/context/settings'
 
 import { defaultSettings, settingsProps, appTabs } from '@/constants'
 import { getTimeFormatter } from '@/helpers'
-const Settings = React.lazy(() => import('@/containers/Settings'))
-const Chat = React.lazy(() => import('../Chat'))
-const NavBar = React.lazy(() => import('@/components/NavBar'))
+
+const Chat = React.lazy(() => import(/* webpackChunkName: "content", webpackPrefetch: true */ '../Chat'))
+const Settings = React.lazy(() => import(/* webpackChunkName: "content" , webpackPrefetch: true */ '@/containers/Settings'))
+const NavBar = React.lazy(() => import(/* webpackChunkName: "content" */ '@/components/NavBar'))
 
 const App: React.FunctionComponent<{}> = function () {
   const updateSettings: { [key: string]: React.Dispatch<React.SetStateAction<string>> } = {}
