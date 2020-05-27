6cb297decc9970c8dc5260369aea90121d196e30
Silverium <soldeplatadeveloper@gmail.com>
2020-05-27 16:50:07 +0200

    remove comment

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 
 import Loading from './src/components/Loading'
 
-// import App from './src/containers/App'
 const App = React.lazy(() => import(/* webpackChunkName: "content"  */ './src/containers/App'))
 
 ReactDOM.render(
