cc3551e70e5d5ec8e821e82ea475e47da2e868fd
Silverium <soldeplatadeveloper@gmail.com>
2020-05-27 19:16:36 +0200

    scroll to bottom

--- src/containers/Chat/index.tsx
+++ src/containers/Chat/index.tsx
@@ -6,7 +6,7 @@ import {
 } from '@/socket'
 
 import { SettingsContext } from '@/context/settings'
-import { settingsProps as settingsConst } from '@/constants'
+import { settingsProps } from '@/constants'
 import SendIcon from '@/components/SendIcon'
 import Button from '@/components/Button'
 
@@ -20,14 +20,16 @@ const Chat: React.FunctionComponent<{messagesList:SocketMessage[], bodyHeight: n
   const { settings } = React.useContext(SettingsContext)
   const onKeyPress = (event: React.KeyboardEvent) => {
     const { key, ctrlKey } = event
-    const userName = settings[settingsConst.USER_NAME]
-    const isCtrlEnterActivated = settings[settingsConst.SEND_ENTER] === 'true'
+    const userName = settings[settingsProps.USER_NAME]
+    const isCtrlEnterActivated = settings[settingsProps.SEND_ENTER] === 'true'
     if (isCtrlEnterActivated && key === 'Enter' && ctrlKey) send({ msg, userName })
   }
   const [msgsHeight, setMsgsHeight] = React.useState(0)
   const footerRef = React.useRef(null)
+  const msgsRef = React.useRef(null)
   React.useEffect(() => {
     setMsgsHeight(bodyHeight - footerRef.current.clientHeight)
+    msgsRef.current.scrollTop = msgsRef.current.scrollHeight
   })
   // TODO: extract messages section with  ul/li formatting
   // TODO: transform and extract "send" section into a small form
@@ -35,12 +37,12 @@ const Chat: React.FunctionComponent<{messagesList:SocketMessage[], bodyHeight: n
     <SettingsContext.Consumer>
       {({ settings, timeFormatter }) => (
         <section>
-          <ul style={{ height: msgsHeight }} className='overflow-y-auto'>
+          <ul ref={msgsRef} style={{ height: msgsHeight }} className='overflow-y-auto'>
 
             {messagesList.map(({ userName, msg, id, timestamp }, i) => (
               <li key={i} className={`m-3 ${socket.id === id ? 'text-right' : 'text-left'}`}>
-                <div>{userName || id}, {timeFormatter(timestamp)}</div>
-                <p> {msg}</p>
+                <div className='my-2'>{userName || id}, {timeFormatter(timestamp)}</div>
+                <span className={`p-1 rounded-4 border-1 border-color-${settings[settingsProps.THEME]}`}> {msg}</span>
               </li>
             ))}
           </ul>
