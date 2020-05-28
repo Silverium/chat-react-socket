92a5a84eaa87a255c71c1b226c1c4e6b03d195ba
Silverium <soldeplatadeveloper@gmail.com>
2020-05-28 13:05:21 +0200

    layout fixes

--- src/components/SendIcon/index.tsx
+++ src/components/SendIcon/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
  * Send icon from https://materialdesignicons.com/
  */
 const SendIcon: React.FunctionComponent<{}> = () => (
-  <svg style={{ width: 24, height: 24, rotate: 'z -55deg' }} viewBox='0 0 24 24'>
+  <svg transform='rotate(-55)' style={{ width: 24, height: 24 }} viewBox='0 0 24 24'>
     <path fill='currentColor' d='M2,21L23,12L2,3V10L17,12L2,14V21Z' />
   </svg>
 )
